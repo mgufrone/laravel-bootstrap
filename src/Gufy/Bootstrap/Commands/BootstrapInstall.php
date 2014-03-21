8ec0dc3a067a09aec5769a9cbc54846a09e6304c
Mochamad Gufron <mgufronefendi@gmail.com>
2014-03-22 02:20:44 +0700

    extend some functionalities

--- src/Gufy/Bootstrap/Commands/BootstrapInstall.php
+++ src/Gufy/Bootstrap/Commands/BootstrapInstall.php
@@ -38,22 +38,31 @@ class BootstrapInstall extends Command {
 	public function fire()
 	{
 		//
-		$structure = __DIR__ . '/../../../../structure';
+		$asset = $this->argument('asset');
+		$structure = __DIR__ . '/../../../../structure/'.$asset;
+		if(!file_exists($structure))
+			$this->error('Asset '.$asset.' not found. ');
         $base = base_path();
 
         $this->line('');
-        $this->line('Installing bootstrap assets for your apps.');
+        $this->line('Installing '.$asset.' assets for your apps.');
         $this->line('');
 
         $this->xcopy(realpath($structure), realpath($base));
 
-        $this->line('');
-        $this->line('Rewriting application.js');
-        $this->writeJs($base);
+        if(file_exists($structure.'/javascripts'))
+        {
+	        $this->line('');
+	        $this->line('Registering '.$asset.' to application.js');
+	        $this->writeJs($asset, $base);
+        }
 
-        $this->line('');
-        $this->line('Rewriting application.css');
-        $this->writeCss($base);
+        if(file_exists($structure.'/stylesheets'))
+        {
+	        $this->line('');
+	        $this->line('Registering '.$asset.' to application.css');
+	        $this->writeCss($asset, $base);
+        }
 
         $this->line('');
         $this->line('Done. Happy Coding! :-)');
@@ -75,7 +84,7 @@ class BootstrapInstall extends Command {
         }
     }
 
-    private function writeJs($base_path)
+    private function writeJs($asset, $base_path)
     {
     	$file = $base_path.'/app/assets/javascripts/application.js';
     	$contents = file_get_contents($file);
@@ -84,9 +93,14 @@ class BootstrapInstall extends Command {
 
 		foreach($data as &$string)
 		{
+			if($string == "//= require ".$asset)
+			{
+				$this->info("Javascript Asset of {$asset} already registered, Ignoring");
+				break;
+			}
 			if($string == '//= require_tree .')
 			{
-				$string = "//= require bootstrap\n".$string;
+				$string = "//= require {$asset} \n".$string;
 			}	
 		}
 
@@ -95,7 +109,7 @@ class BootstrapInstall extends Command {
 		return file_put_contents($file, $contents);
     }
 
-    private function writeCss($base_path)
+    private function writeCss($asset, $base_path)
     {
     	$file = $base_path.'/app/assets/stylesheets/application.css';
     	$contents = file_get_contents($file);
@@ -104,9 +118,14 @@ class BootstrapInstall extends Command {
 
 		foreach($data as &$string)
 		{
+			if($string == " *= require ".$asset)
+			{
+				$this->info("Stylesheet Asset of {$asset} already registered, Ignoring");
+				break;
+			}
 			if(trim($string) == '*= require_tree .')
 			{
-				$string = " *= require bootstrap \n".$string;
+				$string = " *= require ".$asset." \n".$string;
 			}	
 		}
 
@@ -114,4 +133,23 @@ class BootstrapInstall extends Command {
 
 		return file_put_contents($file, $contents);
     }
+
+    protected function getArguments()
+	{
+		return array(
+			array('assets', InputArgument::REQUIRED, 'Asset you want to be published. Ex: bootstrap'),
+		);
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			// array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+		);
+	}
 }
---
+++ src/Gufy/Bootstrap/Commands/BootstrapList.php
@@ -0,0 +1,38 @@
+<?php namespace Gufy\Bootstrap\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class BootstrapList extends Command {
+
+	protected $name = 'bootstrap:list';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'List of available asset that can be registered.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function fire()
+	{
+		$structure = __DIR__ . '/../../../../structure';
+		foreach(scandir($structure) as $dir)
+		{
+			$this->info($dir);
+		}
+		$this->info('To install asset, type php artisan bootstrap:install asset_name');
+	}
+
+}
