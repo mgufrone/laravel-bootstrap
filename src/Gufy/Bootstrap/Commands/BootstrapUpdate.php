cc46052a2a12995a99690ee46e738ace11bd8f5a
Mochamad Gufron <mgufronefendi@gmail.com>
2014-05-15 19:46:35 +0700

    add mass update feature

--- src/Gufy/Bootstrap/Commands/BootstrapUpdate.php
+++ src/Gufy/Bootstrap/Commands/BootstrapUpdate.php
@@ -39,6 +39,10 @@ class BootstrapUpdate extends Command {
 	{
 		//
 		$asset = $this->argument('assets');
+		if(empty($asset))
+		{
+			return $this->massUpdate();
+		}
 		$structure = __DIR__ . '/../../../../structure/'.$asset;
 		if(!file_exists($structure))
 			$this->error('Asset '.$asset.' not found. ');
@@ -73,7 +77,7 @@ class BootstrapUpdate extends Command {
     protected function getArguments()
 	{
 		return array(
-			array('assets', InputArgument::REQUIRED, 'Asset you want to be published. Ex: bootstrap'),
+			array('assets', InputArgument::OPTIONAL, 'Asset you want to be published. Ex: bootstrap'),
 		);
 	}
 
@@ -87,5 +91,52 @@ class BootstrapUpdate extends Command {
 		return array(
 			// array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
 		);
+	}
+
+	private function massUpdate()
+	{
+		$this->line("Let me get what you're already installed");
+		$structure = __DIR__ . '/../../../../structure/';
+		$provider_list = array();
+
+		foreach(scandir($structure) as $dir)
+			if($dir != '.' && $dir != '..')
+				$provider_list[] = $dir;
+
+		$this->line("Looking at application.js");
+		$file = $base_path.'/app/assets/javascripts/application.js';
+    	$contents = file_get_contents($file);
+
+		$data = explode("\n",$contents);
+
+		foreach($data as $line)
+		{
+			if(in_array($asset = str_replace("//= require ", "", $line), $provider_list))
+			{
+				$packages[] = $asset;
+			}
+		}
+
+		$this->line("Looking at application.css");
+		$file = $base_path.'/app/assets/stylesheets/application.css';
+    	$contents = file_get_contents($file);
+
+		$data = explode("\n",$contents);
+
+		foreach($data as $line)
+		{
+			if(in_array($asset = str_replace(" *= require ", "", $line), $provider_list))
+			{
+				$packages[] = $asset;
+			}
+		}
+
+		$this->line("Found. Updating ".implode(",", $packages));
+		foreach($packages as $package)
+		{
+			$this->call('bootstrap:update',array('assets'=>$package));
+		}
+
+		$this->line('All update is doing fine. Happy Coding');
 	}
 }
